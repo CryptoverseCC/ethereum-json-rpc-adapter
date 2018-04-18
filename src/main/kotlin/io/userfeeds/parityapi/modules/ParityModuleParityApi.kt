faed06dfd12721f0fbcbc76b5ce6a4f30a35418c
ghostbuster91 <kghost0@gmail.com>
2018-04-18 16:25:09 +0200

    [Refactor] Format code

--- src/main/kotlin/io/userfeeds/parityapi/modules/ParityModuleParityApi.kt
+++ src/main/kotlin/io/userfeeds/parityapi/modules/ParityModuleParityApi.kt
@@ -1,7 +1,10 @@
 package io.userfeeds.parityapi.modules
 
 import io.reactivex.Single
-import io.userfeeds.parityapi.*
+import io.userfeeds.parityapi.ParityApi
+import io.userfeeds.parityapi.ParityGenericApi
+import io.userfeeds.parityapi.longToHex
+import io.userfeeds.parityapi.unwrap
 import retrofit2.Retrofit
 
 class ParityModuleParityApi(retrofit: Retrofit) : ParityApi.ParityModule {
@@ -9,14 +12,19 @@ class ParityModuleParityApi(retrofit: Retrofit) : ParityApi.ParityModule {
     private val parityGenericApi by lazy { retrofit.create(ParityGenericApi.ParityModule::class.java) }
 
     override fun getBlockHeaderByNumber(blockNumber: Long): Single<ParityApi.BlockHeaderResult> {
-        return parityGenericApi.getBlockHeaderByNumber(ParityGenericApi.Request(method = "parity_getBlockHeaderByNumber", params = listOf(blockNumber.longToHex())))
+        return parityGenericApi.getBlockHeaderByNumber(
+                ParityGenericApi.Request(
+                        method = "parity_getBlockHeaderByNumber",
+                        params = listOf(blockNumber.longToHex())
+                ))
                 .unwrap()
     }
 
     override fun getEnode(): Single<String> {
-        return parityGenericApi.getEnode(ParityGenericApi.Request(method = "parity_enode"))
+        return parityGenericApi.getEnode(
+                ParityGenericApi.Request(
+                        method = "parity_enode"
+                ))
                 .unwrap()
     }
 }
-
-
